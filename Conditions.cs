6a7975391814dbd8522e2dae5b249187d5bc6f07
Kamran <kamran.niftaliyev@eilink.az>
2024-02-13 23:28:37 +0400

    Conditions

---
+++ Conditions.cs
@@ -0,0 +1,67 @@
+//bool lightIsShine = false;
+
+//if(lightIsShine)
+//{
+//    Console.WriteLine("Light is shine..");
+//}
+//else
+//{
+//    Console.WriteLine("Light is not shine.");
+//}
+
+// ==    5 == 1 -> false , 5 == 5 -> true
+// > , < ,  >= , <=    , 5 > 1 -> true , 5 < 1 - > false, 4 <= 5 -> true , 4 >= 5 -> false
+
+// ++ +1 
+// -- -1   
+
+//int number = 5;
+//number++;
+//Console.WriteLine(number);
+//number--;
+//Console.WriteLine(number);
+
+//bool lightIsShine = true;
+//if (lightIsShine == true)
+//{
+//    Console.WriteLine("Light is shine..");
+
+//}
+//else
+//{
+//    Console.WriteLine("Light is not shine.");
+//}
+
+//int number1 = 0;
+//int number2 = 5;
+
+//if (number1 > number2)
+//{
+//    Console.WriteLine(" number1 > " + number2);
+//}
+//else if (number1 > number1)
+//{
+//    Console.WriteLine(" number2 > " + number1);
+//}
+//else if(number1 == number2)
+//{
+//    Console.WriteLine($"{number1} = {number2}    .............");
+//}
+//else
+//{
+//    Console.WriteLine("something is wrong...");
+//}
+
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"number is {number}");
+
+
+//string number = Console.ReadLine(); //abc123
+//if(int.TryParse(number, out int result))
+//{
+//    Console.WriteLine(result);
+//}
+//else
+//{
+//    Console.WriteLine("something is wrong ....");
+//}
