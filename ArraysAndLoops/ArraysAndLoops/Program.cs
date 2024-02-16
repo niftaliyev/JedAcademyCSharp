6a86479b1414707c61f88a1811b85d27170cd081
Kamran <kamran.niftaliyev@eilink.az>
2024-02-16 21:14:02 +0400

    switch case arrays loops

---
+++ ArraysAndLoops/ArraysAndLoops/Program.cs
@@ -0,0 +1,92 @@
+﻿//int[] arr = null;
+
+
+//int[] arr = { 1, 2, 3, 4, 5 };
+//Console.WriteLine(arr[4]);
+
+
+//int[] array = new int[5];
+//array[0] = 555;
+//Console.WriteLine(array[0]);
+
+//int[] arr = new int[5] { 1, 2, 3, 4, 5 };
+
+
+
+
+//int[] arr = { 1, 2, 3, 4, 5 };
+//for (int index = 0; index < 5; index++)
+//{
+//    Console.WriteLine(arr[index]);
+//}
+
+//const int length = 5;
+//int[] arr = new int[length] { 1, 2, 3, 4, 5 };
+//for (int index = 0; index < length; index++)
+//{
+//    Console.WriteLine(arr[index]);
+//}
+
+//const int length = 3;
+//int[] numbers = new int[length];
+//for (int i = 0; i < length; i++)
+//{
+//    numbers[i] = (i+1);
+//}
+
+////// print
+//for (int i = 0; i < length; i++)
+//{
+//    Console.WriteLine(numbers[i]);
+//}
+//int length = 5;
+//char[] characters = new char[length];
+//characters[0] = 'S';
+//characters[1] = 'A';
+//characters[2] = 'L';
+//characters[3] = 'A';
+//characters[4] = 'M';
+//for (int i = 0; i < length; i++)
+//{
+//    Console.Write(characters[i]);
+//}
+
+
+
+//const int length = 9;
+//string[] names = new string[length];
+//names[0] = "Fizuli";
+//names[1] = "Ismayil";
+//names[2] = "Naila";
+//names[3] = "Mircefer";
+//names[4] = "Gultac";
+//names[5] = "Nail";
+//names[6] = "Anur";
+//names[7] = "Vusal";
+//names[8] = "Cemile";
+//// &&
+//bool[] status = new bool[length];
+//status[0] = true;
+//status[1] = true;
+//status[2] = false;
+//status[3] = true;
+//status[4] = true;
+//status[5] = false;
+//status[6] = true;
+//status[7] = false;
+//status[8] = true;
+
+//for (int i = 0; i < length; i++)
+//{
+//	switch (status[i])
+//	{
+//		case true:
+//			Console.WriteLine($"{names[i]} Online");
+//            break;
+//			case false:
+//			Console.WriteLine($"{names[i]} Offline");
+
+//            break;
+
+//	}
+//}
---
+++ switchcase/switchcase/Program.cs
@@ -0,0 +1,37 @@
+﻿
+//Random random = new Random();
+//int randomValue = random.Next(5, 100);
+//Console.WriteLine(randomValue);
+//int secondRandomValue =  random.Next(100, 500);
+//Console.WriteLine(secondRandomValue);
+
+
+// Switch case
+int number1 = 20;
+int number2 = 0;
+char character = '/';
+
+switch (character)
+{
+    case '+':
+        Console.WriteLine(number1+number2);
+        break;
+    case '-':
+        Console.WriteLine(number1 - number2);
+        break;
+    case '/':
+        if(number2 != 0) 
+        { 
+            Console.WriteLine(number1 / number2);
+        }
+        else
+        {
+            Console.WriteLine("Divided By zero");
+        }
+        break;
+    case '*':
+        Console.WriteLine(number1 * number2);
+        break;
+    default:
+        break;
+}
