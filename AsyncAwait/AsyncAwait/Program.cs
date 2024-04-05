eecdcb4198e59e7d6b5b541b104d4dd61ae36b81
Kamran <kamran.niftaliyev@eilink.az>
2024-04-05 21:07:21 +0400

    async await and singleton

---
+++ AsyncAwait/AsyncAwait/Program.cs
@@ -0,0 +1,87 @@
+﻿public static class Program
+{
+    public static void Main()
+    {
+        Start();
+        Loop();
+    }
+
+    static async void Start()
+    {
+        Console.WriteLine("Start in method");
+        var result = await GetResultAsync();
+        Console.WriteLine(result);
+        Console.WriteLine("Finish in method");
+
+
+
+        //Console.WriteLine("Start");
+        //var result = GetResultSync();
+        //Console.WriteLine(result);
+        //Console.WriteLine("Finish");
+
+
+        //Console.WriteLine("Start");
+        //GetResultThread();
+        //Console.WriteLine("Finish");
+
+        //Console.WriteLine("Start");
+        //GetResultTask();
+        //Console.WriteLine("Finish");
+
+
+    }
+
+    static Task<int> GetResultAsync()
+    {
+        return Task.Run(() => 
+        {
+            Thread.Sleep(2500);
+            var result = 42;
+            return result;
+        });
+    }
+
+
+    static void GetResultTask()
+    {
+        Task.Run(() => 
+        {
+            Thread.Sleep(2500);
+            var result = 42;
+            Console.WriteLine($"result is {result}");
+        });
+    }
+
+    static void GetResultThread()
+    {
+        Thread thread = new Thread(() =>
+        {
+            Thread.Sleep(2500);
+            var result = 42;
+            Console.WriteLine($"result is {result}");
+        });
+        thread.Start();
+    }
+
+    static int GetResultSync()
+    {
+        Thread.Sleep(2500);
+        var result = 42;
+        return result;
+    }
+
+
+
+
+
+    public static void Loop()
+    {
+        int x = 0;
+        while (true)
+        {
+            Console.WriteLine(++x);
+            Thread.Sleep(600);
+        }
+    }
+}
---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,46 @@
+﻿public static class Program
+{
+    public static void Main()
+    {
+        Singleton.Instance.Data.Add(1);
+        Singleton.Instance.Data.Add(2);
+        Singleton.Instance.Data.Add(3);
+        Singleton.Instance.Data.Add(33);
+
+        foreach (var item in Singleton.Instance.Data)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine(); Console.WriteLine();
+        Singleton.Instance.Data.Remove(1);
+        Singleton.Instance.Data.Remove(2);
+        foreach (var item in Singleton.Instance.Data)
+        {
+            Console.WriteLine(item);
+        }
+        //Singleton singleton = new Singleton();
+    }
+
+
+    class Singleton
+    {
+        static private Singleton instance = null;
+        public List<int> Data { get; set; }
+        static public Singleton Instance
+        {
+            get 
+            { 
+                if (instance == null)
+                    instance = new Singleton();
+
+                return instance;
+            
+            }
+        }
+        private Singleton()
+        {
+            Data = new List<int>();
+        }
+
+    }
+}
